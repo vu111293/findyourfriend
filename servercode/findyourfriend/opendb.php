6eb65f61cd9d8f1a0cedd8c876d66b506687e2c3
Lê Quốc Vũ <vu111293@users.noreply.github.com>
2014-09-30 10:03:44 +0700

    Update opendb.php

--- servercode/findyourfriend/opendb.php
+++ servercode/findyourfriend/opendb.php
@@ -1,26 +1,8 @@
 <?php
-	//---000webhost
-	/*$host="mysql6.000webhost.com";
-	$user="a1196806_sgudev";
-	$pass="sgu123456";
-	$db="a1196806_sgudev";*/
-	
-	//---orgfree.com
-	/*$host="localhost";
-	$user="839807";
-	$pass="flamingo1";
-	$db="839807";*/
-	
-	//---uni.cc
-	/*$host="sql205.oni.cc";
-	$user="onic_15172818";
-	$pass="flamingo1";
-	$db="onic_15172818_sgulab";*/
-	
 	$host="localhost";
-	$user="sgulabna_01";
-	$pass="v1t2h3vutoanhung";
-	$db="sgulabna_fyf";
+	$user="your user name";
+	$pass="your password";
+	$db="your db name";
 	
 	$con=mysql_connect($host, $user, $pass) or die("Not connect");
 	mysql_query("SET time_zone='+07:00'");
